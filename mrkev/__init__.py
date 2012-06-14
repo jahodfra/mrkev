fecce4db39fcec7a4dbac4de3052dab2a2468f35
Frantisek Jahoda <frantisek@debian.fractal2003.cz>
2012-06-14 17:31:23 +0200

    adding Parser to package namespace

--- mrkev/__init__.py
+++ mrkev/__init__.py
@@ -13,4 +13,4 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 '''
 
 from mrkev.interpreter import Template
-from mrkev.parser import MarkupSyntaxError
+from mrkev.parser import MarkupSyntaxError, Parser
